f282ecd080c4057a1084877b8fea90821e3e0847
Khairun <shaikkhairunnisa99@gmail.com>
2024-06-17 13:21:49 +0530

    backend added

---
+++ Backend/Store_Pro/nexux/src/main/java/com/ts/nexux/NexuxApplication.java
@@ -0,0 +1,13 @@
+package com.ts.nexux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuxApplication.class, args);
+	}
+
+}
