c645d912cb5102fdd4368a08458660bd3160c83a
Tharun G <tharungundela@gmail.com>
2024-06-17 19:54:18 +0530

    products and customer part added

--- Backend/Store_Pro/nexux/src/main/java/com/ts/nexux/NexuxApplication.java
+++ Backend/Store_Pro/nexux/src/main/java/com/ts/nexux/NexuxApplication.java
@@ -2,8 +2,16 @@ package com.ts.nexux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+//@EnableJpaRepositories(basePackages ="com.dao")
+@EntityScan(basePackages="com.model")
+@SpringBootApplication(scanBasePackages ="com")
+@ComponentScan(basePackages = {"com.controller", "dao"})
+@EnableJpaRepositories(basePackages = "dao")
+
 public class NexuxApplication {
 
 	public static void main(String[] args) {
--- Backend/Store_Pro/nexux/src/main/java/dao/CustomerDao.java
+++ Backend/Store_Pro/nexux/src/main/java/dao/CustomerDao.java
@@ -1,75 +1,115 @@
 package dao;
 
 import java.util.List;
-import javax.mail.MessagingException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
+
 import com.model.Customer;
-import utils.EmailUtil;
-import utils.OtpUtil;
+
+//import java.util.List;
+//import javax.mail.MessagingException;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import com.model.Customer;
+
 
 @Service
 public class CustomerDao {
 
-    @Autowired
-    private CustomerRepository customerRepository;
-    
-    @Autowired
-    private OtpUtil otpUtil;
-    
-    @Autowired
-    private EmailUtil emailUtil;
-    
-    @Autowired
-    private PasswordEncoder passwordEncoder; // Inject PasswordEncoder
+	public List<Customer> getAllCustomer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public List<Customer> getAllCustomer() {
-        return customerRepository.findAll();
-    }
+	public Customer getCustomerByEmail(String emailId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public Customer registerCustomer(Customer customer) {
-        String encryptedPassword = passwordEncoder.encode(customer.getPassword());
-        customer.setPassword(encryptedPassword);
-        return customerRepository.save(customer);
-    }
+	public Customer getCustomerById(int cId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public Customer customerLogin(String emailId, String password) {
-        Customer customer = customerRepository.findByEmail(emailId);
-        if (customer != null) {
-            if (passwordEncoder.matches(password, customer.getPassword())) {
-                return customer;
-            }
-        }
-        return null;
-    }
+	public Customer registerCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public String sendOtpToEmail(String emailId) {
-        Customer customer = customerRepository.findByEmail(emailId);
-        String otp = otpUtil.generateOtp();
-        if (customer != null) {    
-            try {
-                emailUtil.sendOtpEmail(customer.getEmail(), customer.getFullName(), otp);
-            } catch (MessagingException e) {
-                throw new RuntimeException("Unable to Send OTP", e);
-            }
-        } else {
-            return "Customer Not Found!!!...";
-        }
-        return otp;
-    }
+	public Customer customerLogin(String emailId, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public Customer getCustomerByEmail(String emailId) {
-        return customerRepository.findByEmail(emailId);
-    }
-    
-    public Customer getCustomerById(int cId) {
-        return customerRepository.findById(cId).orElse(null);
-    }
+	public String sendOtpToEmail(String emailId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public Customer updateCustomerPassword(Customer customer) {
-        String encryptedPassword = passwordEncoder.encode(customer.getPassword());
-        customer.setPassword(encryptedPassword);
-        return customerRepository.save(customer);
-    }
+	public Customer updateCustomerPassword(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+//    @Autowired
+//    private CustomerRepository customerRepository;
+//    
+//    @Autowired
+//    private OtpUtil otpUtil;
+//    
+//    @Autowired
+//    private EmailUtil emailUtil;
+//    
+//    @Autowired
+//    private PasswordEncoder passwordEncoder; // Inject PasswordEncoder
+//
+//    public List<Customer> getAllCustomer() {
+//        return customerRepository.findAll();
+//    }
+//
+//    public Customer registerCustomer(Customer customer) {
+//        String encryptedPassword = passwordEncoder.encode(customer.getPassword());
+//        customer.setPassword(encryptedPassword);
+//        return customerRepository.save(customer);
+//    }
+//
+//    public Customer customerLogin(String emailId, String password) {
+//        Customer customer = customerRepository.findByEmail(emailId);
+//        if (customer != null) {
+//            if (passwordEncoder.matches(password, customer.getPassword())) {
+//                return customer;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public String sendOtpToEmail(String emailId) {
+//        Customer customer = customerRepository.findByEmail(emailId);
+//        String otp = otpUtil.generateOtp();
+//        if (customer != null) {    
+//            try {
+//                emailUtil.sendOtpEmail(customer.getEmail(), customer.getFullName(), otp);
+//            } catch (MessagingException e) {
+//                throw new RuntimeException("Unable to Send OTP", e);
+//            }
+//        } else {
+//            return "Customer Not Found!!!...";
+//        }
+//        return otp;
+//    }
+//
+//    public Customer getCustomerByEmail(String emailId) {
+//        return customerRepository.findByEmail(emailId);
+//    }
+//    
+//    public Customer getCustomerById(int cId) {
+//        return customerRepository.findById(cId).orElse(null);
+//    }
+//
+//    public Customer updateCustomerPassword(Customer customer) {
+//        String encryptedPassword = passwordEncoder.encode(customer.getPassword());
+//        customer.setPassword(encryptedPassword);
+//        return customerRepository.save(customer);
+//    }
 }
