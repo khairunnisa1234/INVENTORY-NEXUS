dffb9e42bea4cce26349e157d8b7b310d86e8746
Khairun <shaikkhairunnisa99@gmail.com>
2024-06-17 13:15:53 +0530

    login created

--- Frontend/I-nexus/src/app/app.module.ts
+++ Frontend/I-nexus/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutusComponent
+    AboutusComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
